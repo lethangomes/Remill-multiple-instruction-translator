4c5706197fe1f5b88a6a82d7b06ccce1db190f8c
lethangomes <lethangomes@gmail.com>
2023-07-11 15:22:41 -0700

    started working on combine instructions function

--- remill-multi-instruction.cpp
+++ remill-multi-instruction.cpp
@@ -4,11 +4,20 @@
 #include <vector>
 using namespace std;
 
+
+void combine_instructions(vector<string> instructions, string dst)
+{
+    //get everything up and including to the "define" line
+
+    //combine all the code between the "define" line and the 
+}
+
 int main(int argc, char* argv[]){
     string instruction;
     vector<string> instructions;
+    vector<string> translatedInstructions;
     string translateCmd = "remill-lift-16 --arch amd64 --ir_out /dev/stdout --bytes ";
-    string logCmd = " | tee -a output.txt";
+    string logCmd = " > output.txt";
 
     //get input file
     ifstream instructionsInputFile (argv[1]);
@@ -28,14 +37,24 @@ int main(int argc, char* argv[]){
             {
                 instructions[i].pop_back();
             }
+
+            //translates current instruction and saves result to output.txt
             cout << (translateCmd + instructions[i]).c_str() << endl;
-            cout << instructions[i].length() << endl;
             system((translateCmd + instructions[i] + logCmd).c_str());
+
+            //Adds translated instruction to a vector of strings
+            ifstream translatedInstruction ("output.txt");
+            translatedInstructions.push_back("");
+            string currentLine;
+            while(getline (translatedInstruction, currentLine))
+            {
+                translatedInstructions[i] = translatedInstructions[i] + currentLine + "\n";
+            }
+            translatedInstruction.close();
         }
 
-        //combine instructions?
-
-        //output translated instructions to output file
+        //combine instructions
+        combine_instructions(translatedInstructions, "output.txt");
 
         //close input file
         instructionsInputFile.close();
